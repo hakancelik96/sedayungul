395e135fe2db36c1ca89ba96149c0a1095f8692f
hakancelik <hakancelik96@outlook.com>
2019-12-21 20:39:46 +0300

    Updates

---
+++ precache-manifest.fee95e9189dddc78f7544039f8800385.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af50b10c729dec3b89f3351e4bd709ce",
+    "url": "/sedayungul/index.html"
+  },
+  {
+    "revision": "57b6952d7a8f85f51ae4",
+    "url": "/sedayungul/static/css/main.2ae6d109.chunk.css"
+  },
+  {
+    "revision": "43a436b3556c8c74a54b",
+    "url": "/sedayungul/static/js/2.18120a1e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/sedayungul/static/js/2.18120a1e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "57b6952d7a8f85f51ae4",
+    "url": "/sedayungul/static/js/main.b716331d.chunk.js"
+  },
+  {
+    "revision": "c3bd70aff9f73d0bdfe6",
+    "url": "/sedayungul/static/js/runtime-main.2fe371c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sedayungul/static/media/logo.5d5d9eef.svg"
+  }
+]);
